e5dd6f110e661d1e08c04d1c39a03661d15aa450
Aldrin Javier <aldrinjavier@dhcp-168-105-111-38.wireless.manoa.hawaii.edu>
2026-04-14 07:16:51 -1000

    Fix start_url and bust SW cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pomodoro-v1'
+const CACHE = 'pomodoro-v2'
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.json', './icons/icon.svg']
 
 self.addEventListener('install', e => {
